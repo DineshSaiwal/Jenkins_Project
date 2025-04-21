f7bdb59c343b120569cfb764b9b2843219217a41
dineshsaiwal <dineshsaiwal98@gmail.com>
2025-04-21 13:17:26 +0530

    added reports

---
+++ src/main/java/Base_Package/Base_Class.java
@@ -0,0 +1,66 @@
+package Base_Package;
+
+import java.awt.Desktop;
+import java.io.File;
+import java.io.IOException;
+import java.time.Duration;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Parameters;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+public class Base_Class {
+	
+	public static ExtentReports extent;
+	ExtentSparkReporter sparkReporter;  
+	public WebDriver driver;
+	public static  String userdir = System.getProperty("user.dir");
+//======================================={Browser Initialization}===============================================
+	
+	@BeforeSuite
+	public void generateReport() throws IOException{
+		sparkReporter =new ExtentSparkReporter("Automation_Report.html");
+		sparkReporter.loadXMLConfig(new File(userdir+"\\extentconfig.xml"));
+		extent = new ExtentReports();
+	    extent.attachReporter(sparkReporter);	
+	    extent.setSystemInfo("Project_Name", "Test Project");
+	    extent.setSystemInfo("Environment", "LOCAl");}
+	
+	
+	
+	@BeforeClass
+		@Parameters({"Browser_Name", "url"})
+		public void setUp() {
+		   
+		    
+		driver = new ChromeDriver();
+		    
+
+		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		        driver.manage().window().maximize();
+		        driver.get("https://practicetestautomation.com/practice-test-login/");
+		
+		}
+	
+	
+	
+		
+		@AfterSuite
+		public void Close_Browser() throws IOException {
+			extent.flush();
+			driver.quit();
+			Desktop.getDesktop().browse(new File("Automation_Report.html").toURI());
+
+			
+		}
+		
+		
+		
+
+
+
+}
---
+++ src/test/java/Test_Package/Login.java
@@ -0,0 +1,30 @@
+package Test_Package;
+
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.markuputils.ExtentColor;
+import com.aventstack.extentreports.markuputils.MarkupHelper;
+
+import Base_Package.Base_Class;
+import Base_Package.Login_Page;
+
+public class Login extends Base_Class{
+	ExtentTest test;
+	
+	@Test
+	void Test_Login() throws InterruptedException {
+		
+		Login_Page pg=new Login_Page( driver);
+		test = extent.createTest("This is a test project").assignAuthor("Dinesh_Saiwal").assignCategory("Regression");
+		
+	Thread.sleep(1000);
+	pg.EnterUN();
+	test.pass(MarkupHelper.createLabel("Entered UN", ExtentColor.GREEN));
+	Thread.sleep(1000);
+	pg.Password();
+	test.pass(MarkupHelper.createLabel("Entered PWD", ExtentColor.GREEN));	}
+
+	
+	
+}
