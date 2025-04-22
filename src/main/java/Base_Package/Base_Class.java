e084914d432f72ccf961f3a7ea69ba2ffbc92f4f
DineshSaiwal <121330329+DineshSaiwal@users.noreply.github.com>
2025-04-22 15:34:04 +0530

    Update Base_Class.java

--- src/main/java/Base_Package/Base_Class.java
+++ src/main/java/Base_Package/Base_Class.java
@@ -6,6 +6,7 @@ import java.io.IOException;
 import java.time.Duration;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeSuite;
@@ -30,24 +31,30 @@ public class Base_Class {
 	    extent.setSystemInfo("Project_Name", "Test Project");
 	    extent.setSystemInfo("Environment", "LOCAl");}
 	
-	
-	
-	@BeforeClass
-		@Parameters({"Browser_Name", "url"})
-		public void setUp() {
-		   ChromeOptions options = new ChromeOptions();
-			String userDataDir = userdir + "/chromeUserData";
-		     options.addArguments("user-data-dir=" + userDataDir);
-		driver = new ChromeDriver(options);
-		    
 
-		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		        driver.manage().window().maximize();
-		        driver.get("https://practicetestautomation.com/practice-test-login/");
-		
-		}
+
+		@BeforeClass
+	@Parameters({"Browser_Name", "url"})
+	public void setUp() {
+	   ChromeOptions options = new ChromeOptions();
+		String userDataDir = userdir + "/chromeUserData";
+	     options.addArguments("user-data-dir=" + userDataDir);
+	     driver = new ChromeDriver(options);
+	        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+	        driver.manage().window().maximize();
+	        driver.get("https://practicetestautomation.com/practice-test-login/");
+	        }
 	
 	
+//	@BeforeClass
+//		@Parameters({"Browser_Name", "url"})
+//		public void setUp() {
+//		driver = new ChromeDriver();
+//		        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+//		        driver.manage().window().maximize();
+//		        driver.get("https://practicetestautomation.com/practice-test-login/");
+//		}
+//	
 	
 		
 		@AfterSuite
